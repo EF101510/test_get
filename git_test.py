1aaf1ce8d97306befa29bbcc2ff4235af1765aa2
hectorliu <hectorliu1015@gamil.com>
2024-07-05 20:09:50 +0800

    添加讀取檔案方法

--- git_test.py
+++ git_test.py
@@ -1,7 +1,21 @@
 
+from typing import BinaryIO
+
+
 def hello(hi:int):
     for i in range(hi):
         print("Hello World!")
 
 
 hello(6)
+
+
+def open_hi (hihi:int):
+    for i in range(hihi):
+        hihihi:BinaryIO = open("test.txt")
+        for line in hihihi.readlines():
+            print(line)
+        hihihi.close()
+
+open_hi(9)
+
